90b67a289176f916c909824fb6ae9928c16da03d
Ethan <ejpinson1@gmail.com>
2022-01-09 15:54:46 -0600

    testing posgreSQL

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const { Pool } = require('pg');
+
 const app = express()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 })
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
