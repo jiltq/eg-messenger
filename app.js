7e37ae8fd9bd4a6dafb642e7a0793a75b23a4580
Ethan <ejpinson1@gmail.com>
2022-01-09 15:36:49 -0600

    trying to fix the procfile??

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-// create an express app
-const express = require("express")
+const express = require('express')
 const app = express()
 
-// use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static('public'))
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World!</h1>')
 })
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
